fe590412d2d54f176cacbab34c1af0c6120914be
jayden648 <gabrielirawan64@gmail.com>
2025-07-09 21:40:56 +0700

    Update routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,10 @@ use App\Http\Controllers\Customer\CustomerController;
 use App\Http\Controllers\Admin\ServiceController;
 use App\Http\Controllers\Admin\OrderController as AdminOrderController;
 
-Route::get('/', function () {
+Route::get('/clear-cache', function () {
+    Artisan::call('config:clear');
+    Artisan::call('cache:clear');
+    return 'Cache cleared!';
     return view('welcome');
 });
 
